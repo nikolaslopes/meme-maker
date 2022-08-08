19ab58ad6aa93101cd23e0e23a3faa7596402e91
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-08-08 12:05:17 -0400

    :bug: fix deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import { enableAllPlugins } from 'immer'
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './components/App'
-
-enableAllPlugins()
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
